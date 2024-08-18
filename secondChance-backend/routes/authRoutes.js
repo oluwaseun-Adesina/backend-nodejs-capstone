388290c0d444e80fdc61302216ec8bad41d8ce3e
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2024-08-18 16:55:52 +0100

    Update user added

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const connectToDatabase = require('../models/db');
@@ -14,19 +15,19 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
-      //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
-      const db = await connectToDatabase();
-      //Access the `users` collection
-      const collection = db.collection("users");
-      //Check for existing email in DB
-      const existingEmail = await collection.findOne({ email: req.body.email });
+        //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        //Access the `users` collection
+        const collection = db.collection("users");
+        //Check for existing email in DB
+        const existingEmail = await collection.findOne({ email: req.body.email });
         if (existingEmail) {
             logger.error('Email id already exists');
             return res.status(400).json({ error: 'Email id already exists' });
         }
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
-        const email=req.body.email;
+        const email = req.body.email;
         //Save user details
         const newUser = await collection.insertOne({
             email: req.body.email,
@@ -43,13 +44,13 @@ router.post('/register', async (req, res) => {
         //Create JWT
         const authtoken = jwt.sign(payload, JWT_SECRET);
         logger.info('User registered successfully');
-        res.json({ authtoken,email });
+        res.json({ authtoken, email });
     } catch (e) {
         logger.error(e);
         return res.status(500).send('Internal server error');
     }
 });
-    //Login Endpoint
+//Login Endpoint
 router.post('/login', async (req, res) => {
     console.log("\n\n Inside login")
     try {
@@ -63,7 +64,7 @@ router.post('/login', async (req, res) => {
         if (theUser) {
             let result = await bcryptjs.compare(req.body.password, theUser.password)
             //send appropriate message if mismatch
-            if(!result) {
+            if (!result) {
                 logger.error('Passwords do not match');
                 return res.status(404).json({ error: 'Wrong pasword' });
             }
@@ -79,7 +80,7 @@ router.post('/login', async (req, res) => {
             const authtoken = jwt.sign(payload, JWT_SECRET);
             logger.info('User logged in successfully');
             return res.status(200).json({ authtoken, userName, userEmail });
-        //Send appropriate message if user not found
+            //Send appropriate message if user not found
         } else {
             logger.error('User not found');
             return res.status(404).json({ error: 'User not found' });
@@ -87,6 +88,52 @@ router.post('/login', async (req, res) => {
     } catch (e) {
         logger.error(e);
         return res.status(500).json({ error: 'Internal server error', details: e.message });
-      }
+    }
+});
+
+// {Insert it along with other imports} Task 1: Use the `body`,`validationResult` from `express-validator` for input validation
+
+router.put('/update', async (req, res) => {
+    // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        // Task 3: Check if `email` is present in the header and throw an appropriate error message if it is not present
+        const email = req.headers.email;
+
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+        // Task 4: Connect to MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        // Task 5: Find the user credentials in database
+        const existingUser = await collection.findOne({ email });
+        existingUser.updatedAt = new Date();
+
+        // Task 6: Update the user credentials in the database
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+        // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
+        const payload = {
+            user: {
+               id: updatedUser._id.toString(),
+            },
+        };
+        
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        res.json({ authtoken });
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+
+    }
 });
 module.exports = router;
