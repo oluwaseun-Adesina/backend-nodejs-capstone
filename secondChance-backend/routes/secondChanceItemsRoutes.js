b8f20ffb9de438fcb15bc37dccb66a2d9b6ac649
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2024-08-18 21:38:13 +0100

    refactor for linter

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     cb(null, file.originalname) // Use the original file name
   }
-}) 
+})
 
 const upload = multer({ storage })
 // Get all secondChanceItems
