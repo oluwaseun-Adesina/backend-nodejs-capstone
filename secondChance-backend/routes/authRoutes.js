a2a4b47606fef97989938d678ac2f2188dbb1e49
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2024-08-18 16:43:38 +0100

    Login

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -33,13 +33,52 @@ router.post('/register', async (req, res) => {
                 id: newUser.insertedId,
             },
         };
-        
+
         const authtoken = jwt.sign(payload, JWT_SECRET);
         // Task 7: Log the successful registration using the logger
         logger.info('User registered successfully');
         // Task 8: Return the user email and the token as a JSON
-        res.json({ authtoken,email });
+        res.json({ authtoken, email });
     } catch (e) {
-         return res.status(500).send('Internal server error');
+        return res.status(500).send('Internal server error');
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if (!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            //continue other tasks
+            // Task 5: Fetch user details from a database
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+            // Task 6: Create JWT authentication if passwords match with user._id as payload
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+            jwt.sign(user._id, JWT_SECRET)
+        }else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.json({ authtoken, userName, userEmail });
+        // Task 7: Send appropriate message if the user is not found
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+
     }
 });
