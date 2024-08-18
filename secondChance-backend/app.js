79daf8be9eded4e40ef88ad176a6e3f8fd6ea0ae
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2024-08-18 19:35:07 +0100

    refactor for linter

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const pinoLogger = require('./logger')
+const path = require('path')
 
 const connectToDatabase = require('./models/db')
 
@@ -26,7 +27,7 @@ const authRoutes = require('./routes/authRoutes')
 const pinoHttp = require('pino-http')
 const logger = require('./logger')
 
-app.use(pinoHttp({ logger }));
+app.use(pinoHttp({ logger }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Use Routes
@@ -36,12 +37,12 @@ app.use('/api/secondchance/search', searchRoutes)
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error')
-});
+  console.error(err)
+  res.status(500).send('Internal Server Error')
+})
 
-app.get('/', (req,res)=> {
-  res.send("Inside the server")
+app.get('/', (req, res) => {
+  res.send('Inside the server')
 })
 
 app.listen(port, () => {
