7723797c9ad00fbae99ecbd55d7dd516ddd3d9ca
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2024-08-18 19:17:43 +0100

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,11 +1,11 @@
-/*jshint esversion: 8 */
+/* jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
+const { loadData } = require('./util/import-mongo/index');
 
 // routes import
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
